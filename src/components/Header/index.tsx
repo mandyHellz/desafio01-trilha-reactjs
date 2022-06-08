1cebf981938d289d3cffc3c90e39eb8f28957376
Amanda Nunes <carvalho_amanda@hotmail.com>
2022-06-08 16:57:36 -0300

    feat: adjusted fetch with prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <div className={styles.headerContainer}>
+      <a href="/">
+        <img src="./Logo.svg" alt="logo" />
+      </a>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import Head from 'next/head';
 import { GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
 
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,101 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [open, setOpen] = useState<boolean>(false);
+  const totalContent = Number(postsPagination.next_page) - 1;
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  return (
+    <>
+      <Head>
+        <title>spacetraveling.</title>
+      </Head>
 
-//   // TODO
-// };
+      <main className={commonStyles.container}>
+        {postsPagination.results.map((post, index) => (
+          <div
+            key={post.uid}
+            className={
+              index >= totalContent && !open
+                ? `${styles.hide}`
+                : `${styles.show}`
+            }
+          >
+            <div className={styles.postPreview}>
+              <h1>{post.data.title}</h1>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.dateAndAuthor}>
+                <p>{post.first_publication_date}</p>
+                <p>{post.data.author}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+
+        {postsPagination.results.map((_, index) => {
+          return (
+            index >= totalContent && (
+              <div>
+                <button
+                  type="button"
+                  onClick={() => setOpen(!open)}
+                  className={
+                    index >= totalContent ? `${styles.show}` : `${styles.hide}`
+                  }
+                >
+                  {!open && (
+                    <span className="flex items-center gap-2">ver mais</span>
+                  )}
+                </button>
+              </div>
+            )
+          );
+        })}
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByType('post', {
+    fetch: [
+      'post.title',
+      'post.subtitle',
+      'post.author',
+      'post.banner',
+      'post.content',
+    ],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map((post): Post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    results: posts,
+    next_page: response.results_size,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+    redirect: 60 * 30,
+  };
+};
